3d9529bd7cb408ece785a8871f72cf347e274447
jialinhe96 <jialin.he@ithaka.org>
2018-11-28 00:19:50 -0500

    Make hover work

--- tower-defense/src/app/choosebase.component.ts
+++ tower-defense/src/app/choosebase.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'choosebase',
@@ -7,12 +7,31 @@ import { Component } from '@angular/core';
 })
 export class ChoosebaseComponent {
     private showeecs = false
+    private btnChanged = false
 
     public showEecsModal: boolean = false
+
+    private changeBtn(x: number, y: number) {
+        let button = document.getElementById("popover-btn")
+        
+        // button.setAttribute("ngbPopover", "You see, I show up on hover!")
+        // button.setAttribute("triggers", "mouseenter:mouseleave")
+        // button.setAttribute("popoverTitle", "Pop title")
+        // button.style.position = "absolute"
+        console.log(x,y)
+        button.style.left = x - 10 + 'px'
+        button.style.top = y - 10 + 'px'
+        button.style.width = "40px"
+        button.style.height = "40px"
+        // let body = document.getElementById("body")
+        // body.appendChild(button)
+        
+    }
 
     private whenHover(event: any) {
         if (event.layerX >= 624 && event.layerX <= 812 && event.layerY >= 359 && event.layerY <= 498) {
             this.showeecs = true
+            this.changeBtn(event.pageX, event.pageY)
         }
         else {
             this.showeecs = false
