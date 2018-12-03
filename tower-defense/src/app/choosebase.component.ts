aad9ad32ad01ac75a2da482e6a5c53769a123c91
jialinhe96 <jialin.he@ithaka.org>
2018-12-03 14:53:35 -0500

    update tutorial and improve choose base page

--- tower-defense/src/app/choosebase.component.ts
+++ tower-defense/src/app/choosebase.component.ts
@@ -8,6 +8,14 @@ import { Router } from '@angular/router';
 })
 export class ChoosebaseComponent {
     private showeecs = false
+    private showggbrown = false
+    private showduder = false
+    private showbbb = false
+    private showpierpont = false
+    private showdow = false
+    private showchresler = false
+    private showstamps = false
+
     private btnChanged = false
 
     public showEecsModal: boolean = false
@@ -16,30 +24,113 @@ export class ChoosebaseComponent {
         private router: Router
     ){}
 
-    private changeBtn(x: number, y: number) {
-        let button = document.getElementById("popover-btn")
+    // private changeBtn(x: number, y: number) {
+    //     let button = document.getElementById("popover-btn")
         
-        // button.setAttribute("ngbPopover", "You see, I show up on hover!")
-        // button.setAttribute("triggers", "mouseenter:mouseleave")
-        // button.setAttribute("popoverTitle", "Pop title")
-        // button.style.position = "absolute"
-        console.log(x,y)
-        button.style.left = x - 10 + 'px'
-        button.style.top = y - 10 + 'px'
-        button.style.width = "40px"
-        button.style.height = "40px"
-        // let body = document.getElementById("body")
-        // body.appendChild(button)
+    //     // button.setAttribute("ngbPopover", "You see, I show up on hover!")
+    //     // button.setAttribute("triggers", "mouseenter:mouseleave")
+    //     // button.setAttribute("popoverTitle", "Pop title")
+    //     // button.style.position = "absolute"
+    //     button.style.left = x - 10 + 'px'
+    //     button.style.top = y - 10 + 'px'
+    //     button.style.width = "40px"
+    //     button.style.height = "40px"
+    //     // let body = document.getElementById("body")
+    //     // body.appendChild(button)
         
-    }
+    // }
 
     private whenHover(event: any) {
         if (event.layerX >= 624 && event.layerX <= 812 && event.layerY >= 359 && event.layerY <= 498) {
             this.showeecs = true
-            this.changeBtn(event.pageX, event.pageY)
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = false
+            // this.changeBtn(event.pageX, event.pageY)
+        }
+        else if (event.layerX >= 621 && event.layerX <= 938 && event.layerY >= 174 && event.layerY <= 319) {
+            this.showeecs = false
+            this.showggbrown = true
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = false
+        }
+        else if (event.layerX >= 444 && event.layerX <= 558 && event.layerY >= 234 && event.layerY <= 374) {
+            this.showeecs = false
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = true
+            this.showchresler = false
+            this.showstamps = false
+        }
+        else if (event.layerX >= 298 && event.layerX <= 439 && event.layerY >= 227 && event.layerY <= 380) {
+            this.showeecs = false
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = true
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = false
+        }
+        else if (event.layerX >= 44 && event.layerX <= 273 && event.layerY >= 523 && event.layerY <= 602) {
+            this.showeecs = false
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = true
+        }
+        else if (event.layerX >= 236 && event.layerX <= 326 && event.layerY >= 758 && event.layerY <= 876) {
+            this.showeecs = false
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = true
+            this.showstamps = false
+        }
+        else if (event.layerX >= 336 && event.layerX <= 551 && event.layerY >= 646 && event.layerY <= 829) {
+            this.showeecs = false
+            this.showggbrown = false
+            this.showduder = true
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = false
+        }
+        else if (event.layerX >= 78 && event.layerX <= 214 && event.layerY >= 643 && event.layerY <= 825) {
+            this.showeecs = false
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = true
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = false
         }
         else {
             this.showeecs = false
+            this.showggbrown = false
+            this.showduder = false
+            this.showbbb = false
+            this.showpierpont = false
+            this.showdow = false
+            this.showchresler = false
+            this.showstamps = false
         }
     }
 
